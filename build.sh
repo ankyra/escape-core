a89199d3da5ca4ee22709a7d6763a88a39d406f7
Bart Spaans <bart.spaans@gmail.com>
2017-09-17 08:44:06 +0100

    Building ain't easy. Don't depend on root directory name

--- build.sh
+++ build.sh
@@ -4,7 +4,11 @@ set -euf -o pipefail
 
 docker rm src || true
 docker create -v /go/src/github.com/ankyra/ --name src golang:1.9.0 /bin/true
-docker cp "$PWD" src:/go/src/github.com/ankyra
+docker cp "$PWD" src:/go/src/github.com/ankyra/tmp
+docker run --rm --volumes-from src \
+    -w /go/src/github.com/ankyra/ \
+    golang:1.9.0 mv tmp escape-core
+    
 docker run --rm \
     --volumes-from src \
     -w /go/src/github.com/ankyra/escape-core \
