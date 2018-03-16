5e04d955604aa06ed57bc4dee480258ba578fed6
Bart Spaans <bart.spaans@gmail.com>
2018-03-16 17:34:23 +0000

    Implement inline scripts. See ankyra/escape#21.

--- exec_stage_test.go
+++ exec_stage_test.go
@@ -37,7 +37,9 @@ func (s *execSuite) Test_ExecStage_ValidateAndFix_parses_deprecated_Script(c *C)
 			RelativeScript: test[0].(string),
 		}
 		c.Assert(unit.ValidateAndFix(), IsNil)
-		c.Assert(unit.GetAsCommand(), DeepEquals, test[1])
+		cmd, err := unit.GetAsCommand()
+		c.Assert(err, IsNil)
+		c.Assert(cmd, DeepEquals, test[1])
 	}
 }
 
@@ -69,11 +71,16 @@ func (s *execSuite) Test_ExecStage_Eval_no_script_used(c *C) {
 			"test": script.LiftString("testing"),
 		}),
 	}
-	unit := NewExecStageForRelativeScript("test.sh")
+	unit, err := NewExecStageFromDict(map[interface{}]interface{}{
+		"script": "test.sh",
+		"inline": "echo hallo\necho hallo",
+	})
+	c.Assert(err, IsNil)
 	env := script.NewScriptEnvironmentFromMap(globals)
 	newUnit, err := unit.Eval(env)
 	c.Assert(err, IsNil)
 	c.Assert(newUnit.RelativeScript, Equals, "test.sh")
+	c.Assert(newUnit.Inline, Equals, "echo hallo\necho hallo")
 }
 
 func (s *execSuite) Test_ExecStage_Eval_all_fields(c *C) {
